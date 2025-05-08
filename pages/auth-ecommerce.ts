6927df25ea2fc3d5a672e6a89072486d09d3b762
Lucas <10016755+lucqa@users.noreply.github.com>
2025-05-08 16:35:56 -0300

    Adding tsconfig and other changes

--- pages/auth-ecommerce.ts
+++ pages/auth-ecommerce.ts
@@ -4,10 +4,22 @@ export class AuthEcommerce {
 
     readonly page: Page // Mandatory
 
+    // Authentication
     readonly inputEmail: Locator // Input for Email
     readonly inputPassword: Locator // Input for Passsword
     readonly buttonSubmit: Locator // Submit Button
     readonly containerLoginShop: Locator // Login Shop Container
+    readonly linkLogout: Locator // Logout link
+    readonly containerLoginAlert: Locator // Alert container for wrong login credentials
+
+    // Product Container
+    readonly containerAnyProduct: Locator // Locator to find any nth-product
+    readonly nameAnyProduct: Locator // Locator to find any nth-product name
+    readonly priceAnyProduct: Locator // Locator to find any nth-product price
+    readonly buttonAddToCartAnyProduct: Locator // Locator to find any nth-product add to cart button
+    readonly srcAnyProduct: Locator // Locator to find any products thumbnail
+
+    // Shopping Cart Container
 
     constructor(page: Page) {
 
@@ -25,6 +37,25 @@ export class AuthEcommerce {
         this.containerLoginShop = page.locator('//*[@id="loginSection"]')
         // Login Shop Container
 
+        this.linkLogout = page.locator('//*[@id="logout"]')
+        // Link to Logout
+
+        this.containerLoginAlert = page.locator('//*[@class="alert alert-danger"]')
+        // Red Alert container for wrong credentials
+
+        this.containerAnyProduct = page.locator('//*[@class="shop-item"]')
+        // Container for Products to use with nth 0-4
+
+        this.nameAnyProduct = page.locator('//*[@class="shop-item-title"]')
+        // Any Products name to use with nth 0-4
+
+        this.priceAnyProduct = page.locator('//*[@class="shop-item-price"]')
+        // Any Products price to use with nth 0-4
+
+        this.buttonAddToCartAnyProduct = page.getByRole('button', { name: 'ADD TO CART'})
+        // Any Products Addto Cart button to use with nth 0-4
+
+        this.srcAnyProduct = page.locator('//*[@class="shop-item-image"]')
     }
 
     async LoginForm(email: string, password: string) {
@@ -34,6 +65,23 @@ export class AuthEcommerce {
 
         // Fill in password
         await this.inputPassword.fill(password)
+
+    }
+
+    async CallPageAndLogin() {
+
+        // Calling Data
+        const email = process.env.CREDENCIAL_EMAIL as string
+        const password = process.env.CREDENCIAL_PASSWORD as string
+
+        // Loading Ecommerce Page
+        await this.page.goto('auth_ecommerce')
+
+        // Calling LoginForm
+        await this.LoginForm(email, password)
+
+        // Click Submit Button
+        await this.buttonSubmit.click()
 
     }
 }
--- tests/auth-ecommerce.spec.ts
+++ tests/auth-ecommerce.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test"
-import { AuthEcommerce } from "../pages/auth-ecommerce"
+import { AuthEcommerce } from "@Pages/auth-ecommerce"
 
-test.describe("Authentication Page Tests", () => {
+test.describe("Basic Authentication Tests", () => {
 
-    test("Loading the site", async ({ page }) => { 
+    test("Accessing Page and Verify Elements", async ({ page }) => {
 
         // Calling Resources
-        const authEcommerce: AuthEcommerce = new AuthEcommerce (page)
+        const authEcommerce: AuthEcommerce = new AuthEcommerce(page)
 
         // Loading Page
         await page.goto('auth_ecommerce')
@@ -30,10 +30,11 @@ test.describe("Authentication Page Tests", () => {
         await expect(authEcommerce.buttonSubmit).toBeVisible()
 
     })
-    test('Authentication with Valid Credentials', async ({ page }) => { 
+
+    test('Authentication Attempt with Valid Credentials', async ({ page }) => {
 
         // Calling Resources
-        const authEcommerce: AuthEcommerce = new AuthEcommerce (page)
+        const authEcommerce: AuthEcommerce = new AuthEcommerce(page)
 
         // Calling Test Data
         const email = process.env.CREDENCIAL_EMAIL as string
@@ -48,6 +49,118 @@ test.describe("Authentication Page Tests", () => {
         // Click Submit Button
         await authEcommerce.buttonSubmit.click()
 
+        // Assertion Logout link is displayed
+        await expect(authEcommerce.linkLogout).toBeVisible()
+
+        // Assertion Logout link contains the text Log Out
+        await expect(authEcommerce.linkLogout).toContainText('Log Out')
+
     })
+
+    test('Authentication Attempt with Invalid Email', async ({ page }) => {
+
+        // Calling Resources
+        const authEcommerce: AuthEcommerce = new AuthEcommerce(page)
+
+        // Calling Test Data
+        const password = process.env.CREDENCIAL_PASSWORD as string
+
+        // Loading Page
+        await page.goto('auth_ecommerce')
+
+        // Filling Login Form
+        await authEcommerce.LoginForm('123@123.com', password)
+
+        // Click Submit Button
+        await authEcommerce.buttonSubmit.click()
+
+        // Assertion Logout link is NOT displayed
+        await expect(authEcommerce.linkLogout).not.toBeVisible()
+
+        // Assertion Red Alert container is visible
+        await expect(authEcommerce.containerLoginAlert).toBeVisible()
+
+        // Assertion Red Alert Container contains Bad Credentials message 
+        await expect(authEcommerce.containerLoginAlert).toContainText("Bad credentials! Please try again! Make sure that you've registered.")
+
+    })
+
+    test('Authentication Attempt with Invalid Password', async ({ page }) => {
+
+        // Calling Resources
+        const authEcommerce: AuthEcommerce = new AuthEcommerce(page)
+
+        // Calling Test Data
+        const email = process.env.CREDENCIAL_EMAIL as string
+
+        // Loading Page
+        await page.goto('auth_ecommerce')
+
+        // Filling Login Form
+        await authEcommerce.LoginForm(email, '1234567890')
+
+        // Click Submit Button
+        await authEcommerce.buttonSubmit.click()
+
+        // Assertion Logout link is NOT displayed
+        await expect(authEcommerce.linkLogout).not.toBeVisible()
+
+        // Assertion Red Alert container is visible
+        await expect(authEcommerce.containerLoginAlert).toBeVisible()
+
+        // Assertion Red Alert Container contains Bad Credentials message 
+        await expect(authEcommerce.containerLoginAlert).toContainText("Bad credentials! Please try again! Make sure that you've registered.")
+
+    })
+
+    test('Authentication Attempt Without Credentials', async ({ page }) => {
+
+        // Calling Resources
+        const authEcommerce: AuthEcommerce = new AuthEcommerce(page)
+
+        // Loading Page
+        await page.goto('auth_ecommerce')
+
+        // Filling Login Form
+        await authEcommerce.LoginForm('', '')
+
+        // Click Submit Button
+        await authEcommerce.buttonSubmit.click()
+
+        // Assertion Logout link is NOT displayed
+        await expect(authEcommerce.linkLogout).not.toBeVisible()
+
+        // Assertion Red Alert container is visible
+        await expect(authEcommerce.containerLoginAlert).toBeVisible()
+
+        // Assertion Red Alert Container contains Bad Credentials message 
+        await expect(authEcommerce.containerLoginAlert).toContainText("Bad credentials! Please try again! Make sure that you've registered.")
+
+    })
+
+})
+
+test.describe("Basic Ecommerce Tests", () => {
+
+    test('Add an item to cart', async ({ page }) => {
+
+        // Calling Resources
+        const authEcommerce: AuthEcommerce = new AuthEcommerce(page)
+
+        // Calling Page and Logging In
+        await authEcommerce.CallPageAndLogin()
+
+        // Storing first products Data to later assertion
+        const productName_0 = await authEcommerce.nameAnyProduct.nth(0).textContent() // Name
+        const productPrice_0 = await authEcommerce.priceAnyProduct.nth(0).textContent() // Price
+        const productSrc_0 = await authEcommerce.srcAnyProduct.nth(0).getAttribute('src') // thumbnail
+
+        // Adding first product to cart
+        await authEcommerce.buttonAddToCartAnyProduct.nth(0).click()
+
+    })
+
+
+
 
 })
